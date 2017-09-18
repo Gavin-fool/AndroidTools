d7fb423867a1689de695cfb0242a12401720cd3b
gavin.Test <1041055971@qq.com>
2017-09-18 17:22:47 +0800

    日常更新

--- app/src/main/java/com/alier/com/androidtools/GFApplication.java
+++ app/src/main/java/com/alier/com/androidtools/GFApplication.java
@@ -1,6 +1,7 @@
-package com.alier.com.commons;
+package com.alier.com.androidtools;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.alier.com.commons.utils.CrashHandler;
 import com.squareup.leakcanary.LeakCanary;
@@ -8,6 +9,7 @@ import com.squareup.leakcanary.RefWatcher;
 
 /**
  * 应用程序application，主要用于捕获全局异常
+ *
  * @author 作者 : gavin_fool
  * @version 1.0
  * @date 创建时间：2017/7/18 17:02
@@ -16,6 +18,7 @@ import com.squareup.leakcanary.RefWatcher;
 public class GFApplication extends Application {
 
     private static RefWatcher refWatcher;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -23,15 +26,22 @@ public class GFApplication extends Application {
         CrashHandler crashHandler = CrashHandler.getCrashHandler();
         crashHandler.init(getApplicationContext());
         //内存泄漏分析
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
         refWatcher = LeakCanary.install(this);
     }
 
-    public static RefWatcher getRefWatcher() {
-        return refWatcher;
+    /**
+     * 获取全局的refWatcher
+     *
+     * @param context
+     * @return
+     */
+    public static RefWatcher getRefWatcher(Context context) {
+        GFApplication application = (GFApplication) context.getApplicationContext();
+        return application.refWatcher;
     }
 }
